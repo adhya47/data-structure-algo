2511087663c6501bf3a86207f6278fee535b2fc6
adhya47 <adhyakhilosia13@gmail.com>
2025-03-10 10:20:44 +0530

    new segment of c++

---
+++ advance.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<vector>
+#include<map>
+#include<array>
+#include<stack>
+#include<string>
+#include<deque>
+#include<queue>
+#include<algorithm>
+#include <climits>
+using namespace std;
