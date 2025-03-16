07de202ea4c86345edc713b2ea97cfafcff7cba1
adhya47 <adhyakhilosia13@gmail.com>
2025-03-16 19:24:28 +0530

    binary search trees 16 march

--- advance.cpp
+++ advance.cpp
@@ -31,62 +31,63 @@ public:
 };
 
 // Function to insert a node into the BST
-Node* creation(Node* root, int x) {
-    if (!root) {
-        return new Node(x);
+Node* creation(Node* root, int &x,int &maxi) {
+    if (!root||root->data == maxi) {
+        Node* temp=new Node(x);
+        return temp;
     }
     if (x < root->data) {
-        root->left = creation(root->left, x);
+        root->left = creation(root->left, x,maxi);
     } else {
-        root->right = creation(root->right, x);
+        root->right = creation(root->right, x,maxi);
     }
     return root;
 }
 
 // Function to delete a node from BST
-Node* deleteNode(Node* root, int x) {
-    if (!root) return NULL;
+// Node* deleteNode(Node* root, int x) {
+//     if (!root) return NULL;
 
-    if (x < root->data) {
-        root->left = deleteNode(root->left, x);
-    } else if (x > root->data) {
-        root->right = deleteNode(root->right, x);
-    } else {
-        // Node found
+//     if (x < root->data) {
+//         root->left = deleteNode(root->left, x);
+//     } else if (x > root->data) {
+//         root->right = deleteNode(root->right, x);
+//     } else {
+//         // Node found
 
-        // Case 1: Leaf node
-        if (!root->left && !root->right) {
-            delete root;
-            return NULL;
-        }
-        // Case 2: Node with one child
-        else if (!root->left) {
-            Node* temp = root->right;
-            delete root;
-            return temp;
-        } else if (!root->right) {
-            Node* temp = root->left;
-            delete root;
-            return temp;
-        }
-        // Case 3: Node with two children
-        else {
-            Node* temp = root->right;
-            while (temp && temp->left) {
-                temp = temp->left;
-            }
-            root->data = temp->data;
-            root->right = deleteNode(root->right, temp->data);
-        }
-    }
-    return root;
-}
+//         // Case 1: Leaf node
+//         if (!root->left && !root->right) {
+//             delete root;
+//             return NULL;
+//         }
+//         // Case 2: Node with one child
+//         else if (!root->left) {
+//             Node* temp = root->right;
+//             delete root;
+//             return temp;
+//         } else if (!root->right) {
+//             Node* temp = root->left;
+//             delete root;
+//             return temp;
+//         }
+//         // Case 3: Node with two children
+//         else {
+//             Node* temp = root->right;
+//             while (temp && temp->left) {
+//                 temp = temp->left;
+//             }
+//             root->data = temp->data;
+//             root->right = deleteNode(root->right, temp->data);
+//         }
+//     }
+//     return root;
+// }
 
 // Function to print the BST (In-order Traversal)
 void print(Node* root) {
     if (!root) return;
-    print(root->left);
     cout << root->data << " ";
+    print(root->left);
     print(root->right);
 }
 // bool check_bst(Node* root,int &prev){
@@ -109,37 +110,37 @@ void print(Node* root) {
 // minimum_dist(root->right,prev,ans);
 // return ans;
 // }
-void smallest_element(Node* root,int &sum,int k){
-    if(!root)return;
-    smallest_element(root->left,sum,k);
-    k--;
-    if(k){
-        sum+=root->data;
-    }else{
-        return;
-    }
-    smallest_element(root->right,sum,k);
+// void smallest_element(Node* root,int &sum,int k){
+//     if(!root)return;
+//     smallest_element(root->left,sum,k);
+//     k--;
+//     if(k){
+//         sum+=root->data;
+//     }else{
+//         return;
+//     }
+//     smallest_element(root->right,sum,k);
 
-}
+// }
 // Main function
-int main() {
-    int arr[] = {3, 7, 4, 1, 6, 8};
-    Node* root = NULL;
-    for (int i = 0; i < 6; i++) {
-        root = creation(root, arr[i]);  // Corrected function call
-    }
+// int main() {
+//     int arr[] = {3, 7, 4, 1, 6, 8};
+//     Node* root = NULL;
+//     for (int i = 0; i < 6; i++) {
+//         root = creation(root, arr[i]);  // Corrected function call
+//     }
 
-    cout << "BST In-order before deletion: ";
-    print(root);
-    cout << endl;
+//     cout << "BST In-order before deletion: ";
+//     print(root);
+//     cout << endl;
 
-    // Deleting a node
-    int key = 4;
-    root = deleteNode(root, key);
+//     // Deleting a node
+//     int key = 4;
+//     root = deleteNode(root, key);
 
-    cout << "BST In-order after deleting " << key << ": ";
-    print(root);
-    cout << endl;
+//     cout << "BST In-order after deleting " << key << ": ";
+//     print(root);
+//     cout << endl;
 // int prev=INT_MIN;
 // if(check_bst(root,prev)){
 //     cout<<true;
@@ -147,11 +148,23 @@ int main() {
 //     cout<<false;
 // }
 
-int sum=0;
-// int mini=minimum_dist(root,prev,ans);
-// cout<<mini;
-//Sum of kth smallest element
-smallest_element(root,sum,3);
-// cout<<sum;
-    return 0;
+// int sum=0;
+// // int mini=minimum_dist(root,prev,ans);
+// // cout<<mini;
+// //Sum of kth smallest element
+// smallest_element(root,sum,3);
+// // cout<<sum;
+//     return 0;
+// }
+int main(){
+    int pre[]={8,5,1,7,10,12};
+    Node* root = NULL;
+    int maxi=INT_MIN;
+    for (int i = 0; i < 6; i++) {
+       maxi=max(maxi,pre[i]);  // Corrected function call
+    }
+    for (int i = 0; i < 6; i++) {
+         root = creation(root, pre[i],maxi);  // Corrected function call
+     }
+     print(root);
 }
