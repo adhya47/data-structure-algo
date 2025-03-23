695bb26e33b1aafe99e84427a10aba4624e27a89
adhya47 <adhyakhilosia13@gmail.com>
2025-03-23 20:59:39 +0530

    sunday lock in

--- advance.cpp
+++ advance.cpp
@@ -169,3 +169,22 @@ void print(Node* root) {
 //      print(root);
 // }
 //binary search trees
+int height(Node* root) {
+    if (!root) return 0;
+    
+    queue<Node*> q;
+    q.push(root);
+    int height = 0;
+    
+    while (!q.empty()) {
+        int size = q.size();
+        for (int i = 0; i < size; i++) {
+            Node* node = q.front();
+            q.pop();
+            if (node->left) q.push(node->left);
+            if (node->right) q.push(node->right);
+        }
+        height++;
+    }
+    return height;
+}
